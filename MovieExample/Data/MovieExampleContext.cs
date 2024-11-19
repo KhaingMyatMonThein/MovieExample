fb02eb7153297649830905fd62dec9eed7e659f1
Khaing Myat Mon Thein <khaingmyatmonthein54@gmail.com>
2024-11-19 10:11:32 +0630

    First Moive Dashboard and create(crud)

---
+++ MovieExample/Data/MovieExampleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieExample.Models;
+
+namespace MovieExample.Data
+{
+    public class MovieExampleContext : DbContext
+    {
+        public MovieExampleContext (DbContextOptions<MovieExampleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieExample.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MovieExample/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MovieExample.Data;
+using MovieExample.Models;
+
+namespace MovieExample.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly MovieExample.Data.MovieExampleContext _context;
+
+        public CreateModel(MovieExample.Data.MovieExampleContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Movie.Id = Guid.NewGuid().ToString();
+
+            //if (!ModelState.IsValid)
+            //{
+            //    return Page();
+            //}
+
+            if (Movie.Title == "" || Movie.Title == null)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
